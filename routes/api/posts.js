41887f3fb356c369c86fdeec85d70989b072bcb7
Anup DebBarma <sunnyoranup@gmail.com>
2019-05-10 12:39:24 +0530

    Comments feature added create-react-app added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,7 +4,7 @@ const { check, validationResult } = require("express-validator/check");
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 const Post = require("../../models/Post");
-const Profile = require("../../models/Profile");
+
 
 router.post(
   "/",
@@ -83,6 +83,118 @@ router.delete("/:id", auth, async (req, res) => {
     if (error.kind == "ObjectId") {
       return res.status(404).json({ msg: "Post not found" });
     }
+    res.status(500).send("Server Error");
+  }
+});
+//Adding like feature
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
+    if (
+      post.likes.filter(like => like.user.toString() == req.user.userId)
+        .length > 0
+    )
+      return res.status(400).json({ msg: "Post already liked" });
+
+    post = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $push: { likes: { user: req.user.userId } } },
+      { new: true }
+    );
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+//Adding unlike feature
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
+    if (
+      post.likes.filter(like => like.user.toString() == req.user.userId)
+        .length == 0
+    )
+      return res.status(400).json({ msg: "Post has not been liked" });
+
+    post = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $pull: { likes: { user: req.user.userId } } },
+      { new: true }
+    );
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.post(
+  "/comment/:id",
+  [
+    auth,
+    [
+      check("text", "text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.userId).select("-password");
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.userId
+      };
+      let post = await Post.findByIdAndUpdate(
+        req.params.id,
+        {
+          $push: { comments: newComment }
+        },
+        { new: true }
+      );
+
+      res.json(post.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+//Delete comment
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+    console.log(comment);
+    if (comment.user.toString() !== req.user.userId) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    post = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $pull: { comments: comment } },
+      { new: true }
+    );
+
+    res.json(post.comments);
+  } catch (error) {
+    console.error(error.message);
     res.status(500).send("Server Error");
   }
 });
