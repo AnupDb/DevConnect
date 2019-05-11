02b9cd3856140a1826a92e1a6e1baf94bf19061f
Anup DebBarma <sunnyoranup@gmail.com>
2019-05-11 12:26:52 +0530

    Login signup setup complete , React router added

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <a href="index.html">
+          <FontAwesomeIcon icon={faCode} /> DevConnect
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <Link to="profiles.html">Developers</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
